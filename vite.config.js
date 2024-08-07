e0df20d6d713a2ae1651763a71bd4701f8d06f5d
Jonas <mechamojonas@gmail.com>
2024-08-07 12:59:55 -0300

    remove base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://libr4.github.io/scheduling_algorithms/"
 })
